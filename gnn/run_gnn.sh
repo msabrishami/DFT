c303f70f7dc6178af835accaef18049e37cf1d13
Saeed Abrishami <abri442@usc.edu>
2020-10-24 19:19:44 -0700

    next change is logic-sim stafan CS

--- gnn/run_gnn.sh
+++ gnn/run_gnn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Classification task on level
-python gnn.py --weight-dim=32 --depth=10 --objective=lev --model=VanillaGCN --train-circuit=c3540 --test-circuit=c432 --problem=classification --loss=CE --bidirectional
+# python gnn.py --weight-dim=32 --depth=10 --objective=lev --model=VanillaGCN --train-circuit=c3540 --test-circuit=c432 --problem=classification --loss=CE --bidirectional
 
-# Regression task on D_p
+Regression task on D_p
 #python gnn.py --weight-dim=128 --depth=50 --objective=D_p --features=C0,C1,CO,CC0,CC1,gtype --model=LSTMGCN --train-circuit=c3540 --test-circuit=c432 --problem=regression --loss=L1 --sigmoid
