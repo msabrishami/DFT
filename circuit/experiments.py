a78872c3adf8d68493be230cb60a35f51d8adf3f
Ting-Yu <72479248+TingYu0924@users.noreply.github.com>
2020-11-05 22:33:18 -0800

    Revert "Revert "Merge branch 'master' of https://github.com/msabrishami/DFT""
    This reverts commit da55fcc2276cfc6684694ed54b10d1faddee52af.

--- circuit/experiments.py
+++ circuit/experiments.py
@@ -6,20 +6,21 @@ import pdb
 from random import randint
 
 from c432_logic_sim import c432_sim
+from load_circuit import LoadCircuit
 
-def check_c432_logicsim(circuit, total_T=1, mode="ckt"):
+def check_c432_logicsim(circuit, tp=1, mode="ckt"):
     """ ckt and verilog files in all ISCAS valid circuits differ by "N"
     if mode is selected ckt: node numbers are simple integers, no issue
     else if mode is verilog, one "N" will be added to node.num
     """
 
-    for t in range(total_T):
+    for t in range(tp):
         PI_dict = dict()
         PI_list = []
 
         
         PI_num = [x.num for x in circuit.PI]
-        PI_num = [x[1:] for x in PI_num] if mode=="verilog" else PI_num 
+        PI_num = [x[1:] for x in PI_num] if mode=="v" else PI_num 
         for pi in PI_num:
             val = randint(0,1)
             PI_dict[pi] = val
@@ -29,32 +30,39 @@ def check_c432_logicsim(circuit, total_T=1, mode="ckt"):
         circuit.logic_sim(PI_list)
         res_ckt = circuit.read_PO()
         res_ckt_2 = {}
-        if mode == "verilog":
+        if mode == "v":
             for k in res_ckt:
                 res_ckt_2[k[1:]] = res_ckt[k]
             res_ckt = res_ckt_2
         if res_beh != res_ckt:
-            print("Wrong")
-            pdb.set_trace()
+            print("Logicsim does NOT match behavioral simulation" +
+                    "for {}, with {} test patterns ".format(circuit.c_name, tp))
             return False
-        print("True")
     print("Logicsim matches behavioral simulation for {}, with {} test patterns ".format(
-        circuit.c_name, total_T))
+        circuit.c_name, tp))
     return True
 
 
-def exp_check_ckt(mode="ckt"):
-    circuit = Circuit("c432")
-    circuit.read_ckt()
-    circuit.lev()
-    check_c432_logicsim(circuit, 1000)
 
-def exp_check_verilog():
-    circuit = Circuit("c432")
-    circuit.read_verilog()
-    circuit.lev()
-    check_c432_logicsim(circuit, 1000, mode="verilog")
 
+def exp_check_verilog_modelsim():
+    for ckt in ["c17", "c432", "c499", "c880", "c1908"]:
+        print("\nCircuit: " + ckt)
+        circuit = Circuit(ckt)
+        LoadCircuit(circuit, "v")
+        circuit.lev()
+        path = "../data/modelsim/golden_IO_from_verilog/golden_" + ckt + "_10_b.txt"
+        circuit.golden_test(path)
+
+
+def exp_check_c432_behavioral(mode="ckt", tp=100, ):
+    if mode not in ["ckt", "v"]:
+        raise NameError("mode {} is not accepted".format(mode))
+    print("Checking c432 behavioral golden with c432 in {} format".format(mode))
+    circuit = Circuit("c432")
+    LoadCircuit(circuit, mode)
+    circuit.lev()
+    check_c432_logicsim(circuit, tp, mode)
 
 
 def exp_read_v2():
