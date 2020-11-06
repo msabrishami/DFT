527c98e4ba02d2d994efa211c41744b322d13f02
Ting-Yu <72479248+TingYu0924@users.noreply.github.com>
2020-11-05 17:08:55 -0800

    checker_logicsim update

--- circuit/checker_logicsim.py
+++ circuit/checker_logicsim.py
@@ -5,7 +5,7 @@ from circuit import *
 from modelsim_simulator import *
 
 class Checker():
-    def __init__(self, c_name, tp):
+    def __init__(self, c_name, tp_count):
         """ create a circuit and a modelsim simulation
         we have two types of inputs: "gate-level-circuit" and a "verilog"
         verilog can be anything, gate level, behavioral, etc. 
@@ -20,58 +20,124 @@ class Checker():
         - etc. 
         """
         self.c_name = c_name
-        self.tp = tp
-   
+        self.tp_count = tp_count
 
-    # Generate random inputs patterns for circuit
-    # Simulate the random input patterns with modelsim, using Modelsim class
-    # Store the golden results in correct locations
-    # This is simply a wrapper around class Modelsim
-    def run(self, ckt_name, tp_count):
+
+    def modelsim_wrapper(self):
         '''
         Given the circuit name and test pattern count, it will do the logic_sim on our platform and the simulation on ModelSim
         Return the check result between logic_sim and ModelSim
-        We use read_verilog() here
+        #We use read_verilog() here
         '''
-        circuit = Circuit(ckt_name)
+        #Generate random inputs patterns for circuit
+        #Simulate the random input patterns with modelsim, using Modelsim class
+        #Store the golden results in correct locations
+        #This is simply a wrapper around class Modelsim
+        circuit = Circuit(self.c_name)
+        #self.circuit = circuit
         circuit.read_verilog()
         circuit.lev()
         sim = Modelsim()
         sim.project(circuit)
-        sim.gen_rand_tp(tp_count= tp_count)
+        #sim.gen_tb(sim.gen_rand_tp(tp_count= tp_count))
+        sim.gen_rand_tp(tp_count= self.tp_count)
         sim.gen_tb()
         sim.simulation()
-        self.tp_path = os.path.join(sim.path_gold, 'golden_' + circuit.c_name + '_'+ str(tp_count)+ '_b.txt')
-        return # self.check_IO_golden(circuit, self.tp_path)
+        self.tp_path = os.path.join(sim.path_gold, 'golden_' + circuit.c_name + '_'+ str(self.tp_count)+ '_b.txt')
+        #return self.check_IO_golden(circuit, self.tp_path)
 
-    
-    def run_check(self, tp_count):
-        """ """
-        for r, d, f in os.walk(config.CKT_DIR):
-            for file in f:
-                if self.circuit.c_name+'.ckt' in file:
-                    if self.run(self.circuit.c_name, tp_count) == False:
-                        print('same!')
-                    else:
-                        print('not same!')
+    def check_ckt_verilog(self, ckt_format = 'verilog'):
+        #check verilog with golden_IO
+        #check ckt with golden_IO
 
+        circuit = Circuit(self.c_name)
+        if ckt_format == 'verilog':
+            circuit.read_verilog()
+        elif ckt_format == 'ckt':
+            circuit.read_ckt()
+        circuit.lev()
+        if self.check_IO_golden(circuit) == True:
+            print(circuit.c_name +  ckt_format + ' matches golden_IO!')
+        else:
+            print(circuit.c_name + ckt_format + ' does not match golden_IO!')
 
-    
-    
-    # Here we check if circuit.logicsim works ok for ckt formats
-    # We check circuit.logicsim results with our golden-IO
+    def check_PI_PO(self):
+        #Given a circuit name, check the PI/PO pins between verilog and ckt
+        #from http://sportlab.usc.edu/~msabrishami/benchmarks.html
+        #between CKT-658 and verilog
+        #circuit: 1355 has different PI/PO pins
+        #circuit: 17, 432, 499, 880, 1908, 3540, 5315, 6288 has the same PI/PO pins 
+        
+        if self.find_file(config.VERILOG_DIR, self.c_name + '.v') == True and self.find_file(config.CKT_DIR, self.c_name + '.ckt'):
+            circuit_verilog = Circuit(self.c_name)
+            circuit_verilog.read_verilog()
+            circuit_ckt = Circuit(self.c_name)
+            circuit_ckt.read_ckt()
+            if self.get_pin_num(circuit_verilog.PO) == self.get_pin_num(circuit_ckt.PO) and self.get_pin_num(circuit_verilog.PI) == self.get_pin_num(circuit_ckt.PI):
+                print('PI/PO pins between ckt and verilog of {} are the same!'.format(self.c_name))
+                return True
+            else:
+                print('PI/PO pins between ckt and verilog of {} are different!'.format(self.c_name))
+                return False
+        else:
+            print('miss ckt or verilog files for {} circuit'.format(self.c_name))
+            return False
+
+    def get_pin_num(self, node_list):
+        #get the pins number list
+        num_list = []
+        for node in node_list:
+            num_list.append(node.num)
+        return num_list
+
+    def find_file(self, path, fname):
+        #check if the file is exist
+        for r, d, f in os.walk(path):
+            if fname in f:
+                return True
+            else:
+                return False
+
+    def check_IO_golden(self, circuit):
+        #we have golden_test() in circuit
+        #We already generated golden-results
+        #Now we just want to check if circuit.logicsim of ckt or verilog matches
+        infile = open(self.tp_path, "r")
+        lines = infile.readlines()
+        PI_t_order  = [x[1:] for x in lines[0][8:].strip().split(',')]
+        PO_t_order = [x[1:] for x in lines[1][8:].strip().split(',')]
+        PI_num = [x.num for x in circuit.PI]
+        #print("Logic-Sim validation with {} patterns".format(int((len(lines)-2)/3)))
+        if PI_t_order != PI_num:
+            print("Error:{} PI node order does not match! ".format(circuit.c_name))
+            return False
+        for t in range(int((len(lines)-2)/3)):
+            test_in  = [int(x) for x in lines[(t+1)*3].strip().split(',')]
+            test_out = [int(x) for x in lines[(t+1)*3+1].strip().split(',')]
+            circuit.logic_sim(test_in)
+            logic_out = circuit.read_PO()
+            for i in range(len(PO_t_order)):
+                out_node = PO_t_order[i]
+                out_node_golden = test_out[i]
+                if out_node_golden != logic_out["out"+str(out_node)]:
+                    print("Error:{} PO node order does not match! ".format(circuit.c_name))
+                    return False
+        #print("Validation completed successfully - all correct")
+        return True
+    '''
     def run_ckt(self, ckt_name, tp_count):
+
         circuit2 = Circuit(ckt_name)
         circuit2.read_ckt()
         circuit2.lev()
         #tp_path = os.path.join(sim.path_gold, 'golden_' + circuit.c_name + '_'+ str(tp_count)+ '_b.txt')
         return self.check_IO_golden(circuit2, self.tp_path)
-
-
+    '''
+    '''
     def run_all(self, tp_count):
-        '''
-        It will run all of .v files in VERILOG_DIR and compare results between ModelSim and our Simulator
-        '''
+        
+        #It will run all of .v files in VERILOG_DIR and compare results between ModelSim and our Simulator
+        
         file_names = []
         file_names_ckt = []
         #r=root, d=directories, f = files
@@ -109,15 +175,15 @@ class Checker():
         print('############## All Circuits Tests Pass ! ################')
         print('#########################################################')
         return True
-
-
+    '''
+    '''
     def check_PI_PO(self, fname_ckt, fname_verilog):
-        '''
-        from http://sportlab.usc.edu/~msabrishami/benchmarks.html
-        between CKT-658 and verilog
-        circuit: 1355 has different PI/PO pins
-        circuit: 17, 432, 499, 880, 1908, 3540, 5315, 6288 has the same PI/PO pins 
-        '''
+        
+        #from http://sportlab.usc.edu/~msabrishami/benchmarks.html
+        #between CKT-658 and verilog
+        #circuit: 1355 has different PI/PO pins
+        #circuit: 17, 432, 499, 880, 1908, 3540, 5315, 6288 has the same PI/PO pins 
+        
         ckt_path = os.path.join(config.CKT_DIR,fname_ckt)
         verilog_path = os.path.join(config.VERILOG_DIR,fname_verilog)
         fr_ckt = open(ckt_path, mode = 'r')
@@ -167,11 +233,12 @@ class Checker():
             print('{} and {} are the same!'.format(fname_ckt, fname_verilog))
         else:
             print('{} and {} are not the same!'.format(fname_ckt, fname_verilog))
-
-    def check_PI_PO_all(self):
-        '''
-        check all of .ckt and .v files automatically by using function check_PI_PO()
-        '''
+    '''
+    '''
+    def run_check_PI_PO(self):
+        
+        #check all of .ckt and .v files automatically by using function check_PI_PO()
+        
         file_names_ckt = []
         file_names_ckt_and_verilog = []
         # r=root, d=directories, f = files
@@ -190,37 +257,9 @@ class Checker():
         #check by function check_PI_PO()
         for file in file_names_ckt_and_verilog:
             self.check_PI_PO(file + '.ckt', file + '.v')
+    '''
 
-
-     
-    def check_logicsim(self, ckt_format):
-        """ We already generated golden-results 
-        Now we just want to check if circuit.logicsim of ckt or verilog matches
-        """ 
-        # We already have golden_test() in circuit
-
-        infile = open(golden_io_filename, "r")
-        lines = infile.readlines()
-        PI_t_order  = [x[1:] for x in lines[0][8:].strip().split(',')]
-        PO_t_order = [x[1:] for x in lines[1][8:].strip().split(',')]
-        PI_num = [x.num for x in circuit.PI]
-        print("Logic-Sim validation with {} patterns".format(int((len(lines)-2)/3)))
-        if PI_t_order != PI_num:
-            print("Error: PI node order does not match! ")
-            return False
-        for t in range(int((len(lines)-2)/3)):
-            test_in  = [int(x) for x in lines[(t+1)*3].strip().split(',')]
-            test_out = [int(x) for x in lines[(t+1)*3+1].strip().split(',')]
-            circuit.logic_sim(test_in)
-            logic_out = circuit.read_PO()
-            for i in range(len(PO_t_order)):
-                out_node = PO_t_order[i]
-                out_node_golden = test_out[i]
-                if out_node_golden != logic_out["out"+str(out_node)]:
-                    print("Error: PO node order does not match! ")
-                    return False
-        print("Validation completed successfully - all correct")
-        return True
+    
 '''
 try:
     Checker().run_all(50)
