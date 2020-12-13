dd1e342d5c150021e5ae89614c27f3a2f4e04984
Ting-Yu <72479248+TingYu0924@users.noreply.github.com>
2020-12-13 10:39:44 -0800

    update atpg_new

--- circuit/atpg_new.py
+++ circuit/atpg_new.py
@@ -7,83 +7,91 @@ from collections import deque
 from circuit import Circuit
 
 import time
-
-'''
-D     1/0
-D_BAR 0/1
-five value define
-access value:five_value.ZERO.value
-access variable:five_value(0).name
-class five_value(Enum):
-   ZERO = 0
-   ONE = 15
-   D = 12
-   D_BAR = 3
-   X = 9
-gtype:IPT, BRCH, XOR, OR, NOR, NOT, NAND, AND
-ntype:GATE, PI, FB, PO
-'''
+import os, sys
 
 class ATPG:
    def __init__(self, circuit):
       self.circuit = circuit
       
       self.total_fault_list = []
+      self.reduce_fault_list = []
       #self.reduce_fault_list = []
       for node in self.circuit.nodes_lev:
-         #if node.gtype in ['BRCH', 'IPT']:
-            #self.reduce_fault_list.append((node.num,0))
-            #self.reduce_fault_list.append((node.num,1))
          self.total_fault_list.append((node.num,0))
          self.total_fault_list.append((node.num,1))
-
-   #TODO print ATPG information
-   #def __str__(self):
-   #   res = []
-   #   return '\n'.join(res)
+         #self.total_fault_list.insert(0,(node.num,0))
+         #self.total_fault_list.insert(0,(node.num,1))
+      for node in self.circuit.nodes_lev:
+         if node.gtype in ['BRCH', 'IPT']:
+            self.reduce_fault_list.append((node.num,0))
+            self.reduce_fault_list.append((node.num,1))
 
    def atpg_det(self, DFS_PFS = 'DFS', Podem_Dalg = 'Podem'):
       print('#####DETERMINISTIC#####')
       start = time.time()
       self.total_fault_set = set(self.total_fault_list)
       #self.reduce_fault_set = set(self.reduce_fault_list)
+      self.fault_detected = set()
+      self.error_list = []
       #self.circuit.lev()
       self.circuit.SCOAP_CC()
       self.circuit.SCOAP_CO()
       flag = 0
-      count_set = 100
-      while(flag < 2):
-         for fault in self.total_fault_set:
-            if Podem_Dalg == 'Podem':
-               test = Podem(self.circuit, fault[0], fault[1], count_set)
-            else:
-               test = D_alg(self.circuit, fault[0], fault[1], count_set)
-            if test.test() == True: #TODO there should be a wrapper function in Podem and DAlg whose name is test
-               IPT_list = test.return_IPT()
-               IPT_binary_list = []
-               for x in IPT_list:
-                  if x == 15 or x == 12 or x == 9: 
-                     IPT_binary_list.append(1)
-                  else:
-                     IPT_binary_list.append(0)
-               if DFS_PFS == 'DFS':
-                  dfs_test = DFS(self.circuit)
-                  fault_list_set = dfs_test.fs_for_atpg(IPT_binary_list)
+      count_set = 100 #500
+      while(flag < 2 and len(self.total_fault_set) != 0):#len(self.reduce_fault_set) != 0):#len(self.total_fault_set) != 0):
+         fault = self.total_fault_set.pop()
+         #fault = self.reduce_fault_set.pop()
+         #for fault in self.total_fault_set:
+         #print('rest fault list', len(self.total_fault_set))
+         #print('rest fault list', len(self.reduce_fault_set))
+         #print(fault)
+         if Podem_Dalg == 'Podem':
+            test = Podem(self.circuit, fault[0], fault[1], count_set)
+         else:
+            test = D_alg(self.circuit, fault[0], fault[1], count_set)
+         #blockPrint()
+         if test.test() == True:        
+            IPT_list = test.return_IPT()
+            IPT_binary_list = []
+            for x in IPT_list:
+               if x == 15 or x == 12 or x == 9: 
+                  IPT_binary_list.append(1)
                else:
-                  pfs_test = PFS(self.circuit)
-                  fault_list_set = pfs_test.fs_for_atpg(self.total_fault_set, IPT_binary_list)
-               self.total_fault_set = self.total_fault_set - fault_list_set
-
-         count_set += 5000 #TODO there is a count in Podem and DAlg to terminate the search for each fault which is initialized to 100
-         flag += 1
-         if len(self.total_fault_set) == 0:
+                  IPT_binary_list.append(0)
+            if DFS_PFS == 'DFS':
+               dfs_test = DFS(self.circuit)
+               fault_list_set = dfs_test.fs_for_atpg(IPT_binary_list)
+            else:
+               pfs_test = PFS(self.circuit)
+               fault_list_set = pfs_test.fs_for_atpg(self.total_fault_set, IPT_binary_list)
+            self.total_fault_set = self.total_fault_set - fault_list_set
+            #self.reduce_fault_set = self.reduce_fault_set - fault_list_set
+            self.fault_detected = self.fault_detected | fault_list_set
+            if fault in self.error_list:
+               self.error_list.remove(fault)
+            self.error_list = list(set(self.error_list) - fault_list_set)
+         else:
+            if fault in self.error_list:
+               pass
+            else:
+               self.error_list.append(fault)
+         enablePrint()
+         if len(self.total_fault_set) == 0 and len(self.error_list) != 0:
+         #if len(self.reduce_fault_set) == 0 and len(self.error_list) != 0:
+            self.total_fault_set = set(self.error_list)
+            #self.reduce_fault_set = set(self.error_list)
+            flag += 1
+            count_set += 5000 #200000 
+         if len(self.total_fault_set) == 0 and len(self.error_list) == 0:
+         #if len(self.reduce_fault_set) == 0 and len(self.error_list) == 0:
              flag = 2
       end = time.time()
-      #TODO comment
-      print('fault can not be detected >>',self.total_fault_set)
+      
+      print('fault can not be detected >>',self.error_list)
       print('total num of fault >>',len(self.total_fault_list))
-      print('total num of fault can not be detected >>',len(self.total_fault_set))
-      print('fault coverage >>',(len(self.total_fault_list)-len(self.total_fault_set))/len(self.total_fault_list))  
+      print('total num of fault can not be detected >>',len(self.error_list))
+      #print('fault coverage >>',(len(self.total_fault_list)-len(self.error_list))/len(self.total_fault_list))  
+      print('fault coverage >>',len(self.fault_detected)/len(self.total_fault_list))  
       print('run time >>', end - start) 
 
 
@@ -96,7 +104,6 @@ class ATPG:
       self.circuit.SCOAP_CC()
       self.circuit.SCOAP_CO()
       self.total_fault_set = set(self.total_fault_list)
-      #self.reduce_fault_set = set(self.reduce_fault_list)
       while((len(self.total_fault_list)-len(self.total_fault_set))/len(self.total_fault_list) < RAN_percentage/100):
          IPT_binary_list = self.circuit.gen_tp()
          if DFS_PFS == 'DFS':
@@ -107,94 +114,58 @@ class ATPG:
             fault_list_set = pfs_test.fs_for_atpg(self.total_fault_set, IPT_binary_list)
          self.total_fault_set = self.total_fault_set - fault_list_set
       flag = 0
-      count_set = 100
-      while(flag < 2):
-         for fault in self.total_fault_set:
-            if Podem_Dalg == 'Podem':
-               test = Podem(self.circuit, fault[0], fault[1], count_set)
-            else:
-               test = D_alg(self.circuit, fault[0], fault[1], count_set)
-            if test.test() == True: #TODO there should be a wrapper function in Podem and DAlg whose name is test
-               IPT_list = test.return_IPT()
-               IPT_binary_list = []
-               for x in IPT_list:
-                  if x == 15 or x == 12 or x == 9: 
-                     IPT_binary_list.append(1)
-                  else:
-                     IPT_binary_list.append(0)
-               if DFS_PFS == 'DFS':
-                  dfs_test = DFS(self.circuit)
-                  fault_list_set = dfs_test.fs_for_atpg(IPT_binary_list)
+      count_set = 100 #500
+      while(flag < 2 and len(self.total_fault_set) != 0):
+         fault = self.total_fault_set.pop()
+         #print('rest fault list', len(self.total_fault_set))
+         #print(fault)
+         if Podem_Dalg == 'Podem':
+            test = Podem(self.circuit, fault[0], fault[1], count_set)
+         else:
+            test = D_alg(self.circuit, fault[0], fault[1], count_set)
+         #blockPrint()
+         if test.test() == True:
+            IPT_list = test.return_IPT()
+            IPT_binary_list = []
+            for x in IPT_list:
+               if x == 15 or x == 12 or x == 9: 
+                  IPT_binary_list.append(1)
                else:
-                  pfs_test = PFS(self.circuit)
-                  fault_list_set = pfs_test.fs_for_atpg(self.total_fault_set, IPT_binary_list)
-               self.total_fault_set = self.total_fault_set - fault_list_set
-
-         count_set += 5000 #TODO there is a count in Podem and DAlg to terminate the search for each fault which is initialized to 100
-         flag += 1
-         if len(self.total_fault_set) == 0:
+                  IPT_binary_list.append(0)
+            if DFS_PFS == 'DFS':
+               dfs_test = DFS(self.circuit)
+               fault_list_set = dfs_test.fs_for_atpg(IPT_binary_list)
+            else:
+               pfs_test = PFS(self.circuit)
+               fault_list_set = pfs_test.fs_for_atpg(self.total_fault_set, IPT_binary_list)
+            self.total_fault_set = self.total_fault_set - fault_list_set
+            if fault in self.error_list:
+               self.error_list.remove(fault)
+            self.error_list = list(set(self.error_list) - fault_list_set)
+         else:
+            if fault in self.error_list:
+               pass
+            else:
+               self.error_list.append(fault)
+         enablePrint()
+         if len(self.total_fault_set) == 0 and len(self.error_list) != 0:
+            self.total_fault_set = set(self.error_list)
+            flag += 1
+            count_set += 5000 #200000
+         if len(self.total_fault_set) == 0 and len(self.error_list) == 0:
              flag = 2
       end = time.time()
-      #TODO comment
-      print('fault can not be detected >>',self.total_fault_set)
+      
+      print('fault can not be detected >>',self.error_list)
       print('total num of fault >>',len(self.total_fault_list))
-      print('total num of fault can not be detected >>',len(self.total_fault_set))
-      print('fault coverage >>',(len(self.total_fault_list)-len(self.total_fault_set))/len(self.total_fault_list))  
+      print('total num of fault can not be detected >>',len(self.error_list))
+      print('fault coverage >>',(len(self.total_fault_list)-len(self.error_list))/len(self.total_fault_list))  
       print('run time >>', end - start) 
 
-def ATPG_DET(circuit, alg_type):
-   """
-   Stand alone function:
-   execulte DALG or PODEM for a fault based on alg_type
-   when the alg_type is chosen, all faults in the fault set will be exe by this alg
-   """
-   ############## Is it good??????????????????????
-   fault_sim = FaultSim(circuit)
+# Disable
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
 
-   # set: fault cannot be detected
-   fault_non_det = set()
-   fault_set_rest = fault_sim.fault_set_rest
-
-   # record time
-   start_time = time.time()
-
-   if alg_type == "DALG":
-      while fault_set_rest:
-         fault = fault_set_rest.pop()
-         alg_obj = D_alg(circuit, fault[0], fault[1])
-
-         if alg_obj.dalg():
-            IPI_list = alg_obj.return_IPI()
-            IPI_binary_list = []
-            #print(IPT_list)
-            for x in IPI_list:
-               if x == 15 or x == 12 or x == 9: 
-                     IPI_binary_list.append(1)
-               else:
-                     IPI_binary_list.append(0)
-
-            #print(IPT_binary_list)
-            dfs_test = DFS(circuit)
-            fault_subset = dfs_test.single(IPI_binary_list)
-            fault_set_rest = fault_set_rest.difference(fault_subset)
-         else:
-            # if the dalg fails, add the fault to non-det set
-            fault_non_det.add(fault)
-
-      end_time = time.time()
-      exe_time = end_time - start_time
-
-      fault_coverage = (len(circuit.nodes_lev) - len(fault_non_det)) / len(circuit.nodes_lev)
-
-      print("Algorithm: DALG")
-      print("Circuit: ", circuit.c_name)
-      print("Fault Coverage: ", fault_coverage)
-      print("Time: ", exe_time)
-      print("None detectable faults: ", fault_non_det, "\n")
-
-   elif alg_type == "PODEM":
-      raise NameError("PODEM TBC")
-
-   else:
-      raise NameError("Algorithm type is invalid! alg_type = 'DALG' or 'PODEM'!")
-
+# Restore
+def enablePrint():
+    sys.stdout = sys.__stdout__
